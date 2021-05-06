59e86c1700ea1fcb3ec704196c1b1efa7296d5e5
tanykazy <tanykazy@gmail.com>
2021-05-06 23:27:43 +0900

    Add Python dir

---
+++ Python/lifegame.py
@@ -0,0 +1,14 @@
+
+
+def initCells(w, h):
+    cells = [[0 for x in range(w)]for y in range(h)]
+    return cells
+
+
+
+
+
+if __name__ == "__main__":
+    cells = initCells(3, 5)
+    print(cells)
+
