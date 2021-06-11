baaabbb55255a77d0462da72cd0ca5082bf482b5
tanykazy <tanykazy@gmail.com>
2021-06-11 22:56:57 +0900

    Add rand_cell()

--- C/lifegame.c
+++ C/lifegame.c
@@ -1,36 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "lifegame.h"
 
+int main(int argc, char *argv[])
+{
+	board *b;
+	b = make_board(10, 10);
+	print_board(b);
+	free(b->s);
+	free(b);
+	return 0;
+}
 
-typedef char cell;
-typedef cell** cells;
-
-typedef struct {
-	int w;
-	int h;
-	cells s;
-} board;
-
-
-cells make_cells(int w, int h, board* b)
+cells make_cells(int w, int h, board *b)
 {
 	int i;
 	int j;
 	cells cells_pp;
 
-	cells_pp = (cells)malloc(sizeof(cell*) * h);
-	if(cells_pp == NULL)
+	cells_pp = (cells)malloc(sizeof(cell *) * h);
+	if (cells_pp == NULL)
 	{
 		free(b);
 		exit(EXIT_FAILURE);
 	}
 
-	for(i = 0; i < h; i++)
+	for (i = 0; i < h; i++)
 	{
-		cells_pp[i] = (cell*)malloc(sizeof(cell) * w);
-		if(cells_pp[i] == NULL)
+		cells_pp[i] = (cell *)malloc(sizeof(cell) * w);
+		if (cells_pp[i] == NULL)
 		{
-			for(; i > 0; i--)
+			for (; i > 0; i--)
 			{
 				free(cells_pp[i]);
 			}
@@ -39,9 +37,9 @@ cells make_cells(int w, int h, board* b)
 			exit(EXIT_FAILURE);
 		}
 
-		for(j = 0; j < w; j++)
+		for (j = 0; j < w; j++)
 		{
-			cells_pp[i][j] = 0;
+			cells_pp[i][j] = rand_cell();
 		}
 	}
 
@@ -50,8 +48,8 @@ cells make_cells(int w, int h, board* b)
 
 board *make_board(int w, int h)
 {
-	board* board_p = (board*)malloc(sizeof(board));
-	if(board_p == NULL)
+	board *board_p = (board *)malloc(sizeof(board));
+	if (board_p == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
@@ -63,13 +61,27 @@ board *make_board(int w, int h)
 	return board_p;
 }
 
-int main(int argc, char* argv[])
+cell rand_cell(void)
 {
-	board *b;
-	b = make_board(10, 10);
-
-	free(b->s);
-	free(b);
-	return 0;
+	int c = rand() % 2;
+	return (cell)c;
 }
 
+void print_board(board *b)
+{
+	for (int w = 0; w < b->w; w++)
+	{
+		for (int h = 0; h < b->h; h++)
+		{
+			if (b->s[h][w] == 0)
+			{
+				printf("%c", ' ');
+			}
+			else
+			{
+				printf("%c", '*');
+			}
+		}
+		printf("%c", '\n');
+	}
+}
