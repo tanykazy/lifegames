c8665b55691b9fcb1658af1caeadccdf75bf63bd
tanykazy <tanykazy@gmail.com>
2021-06-13 00:00:08 +0900

    Print next step

--- C/lifegame.c
+++ C/lifegame.c
@@ -2,26 +2,127 @@
 
 int main(int argc, char *argv[])
 {
-	struct board *b_p = NULL;
+	unsigned int seed;
 	result r;
+	struct board *b_p = NULL;
 
-	r = make_board(&b_p, 8, 7);
+	seed = time(NULL);
+	srand(seed);
+
+	r = make_board(&b_p, 20, 20);
 	if (r == FAILURE)
 	{
 		return EXIT_FAILURE;
 	}
 
-	print_board(b_p);
+	// b_p->s->c[0][0] = 1;
+	// b_p->s->c[0][1] = 1;
+	// b_p->s->c[0][2] = 1;
+	// b_p->s->c[1][0] = 1;
+	// b_p->s->c[2][1] = 1;
 
-	free_state(b_p->s);
+	for (int i = 0; i < 100; i++)
+	{
+		print_board(b_p);
+		next(b_p);
+		// printf("%c", '\n');
+	}
+
+	free_state(b_p);
 	free(b_p);
 	return EXIT_SUCCESS;
+}
+
+result next(struct board *b_p)
+{
+	int i;
+	int j;
+	int c;
+	result r;
+	cells next;
+	cells current = b_p->s->c;
+	int w = b_p->w;
+	int h = b_p->h;
+	struct state *s = NULL;
+
+	r = make_state(&s, w, h);
+	if (r == FAILURE)
+	{
+		return FAILURE;
+	}
+
+	next = s->c;
+
+	for (i = 0; i < w; i++)
+	{
+		for (j = 0; j < h; j++)
+		{
+			c = count_neighbor(i, j, b_p);
+			if (current[i][j] > 0)
+			{
+				if (c == 2 || c == 3)
+				{
+					next[i][j] = 1;
+				}
+				else
+				{
+					next[i][j] = 0;
+				}
+			}
+			else
+			{
+				if (c == 3)
+				{
+					next[i][j] = 1;
+				}
+				else
+				{
+					next[i][j] = 0;
+				}
+			}
+		}
+	}
+
+	free_state(b_p);
+	b_p->s = s;
+	return SUCCESS;
+}
+
+int count_neighbor(int x, int y, struct board *b)
+{
+	int i;
+	int j;
+	int n = 0;
+
+	for (i = -1; i < 2; i++)
+	{
+		for (j = -1; j < 2; j++)
+		{
+			if (i != 0 || j != 0)
+			{
+				if (toroidalize_cell(x + i, y + j, b) > 0)
+				{
+					n++;
+				}
+			}
+		}
+	}
+
+	return n;
+}
+
+cell toroidalize_cell(int x, int y, struct board *b)
+{
+	x += b->w;
+	y += b->h;
+	x %= b->w;
+	y %= b->h;
+	return b->s->c[x][y];
 }
 
 result make_board(struct board **b_pp, int w, int h)
 {
 	struct board *b = NULL;
-	unsigned int seed;
 	result r;
 
 	b = (struct board *)malloc(sizeof(struct board));
@@ -30,22 +131,12 @@ result make_board(struct board **b_pp, int w, int h)
 		return FAILURE;
 	}
 
-	seed = time(NULL);
-	srand(seed);
-
 	b->w = w;
 	b->h = h;
-	r = make_state(&b->s);
-	if (r == FAILURE)
-	{
-		free(b);
-		return FAILURE;
-	}
 
-	r = make_cells(b->s, w, h);
+	r = make_state(&b->s, w, h);
 	if (r == FAILURE)
 	{
-		free(b->s);
 		free(b);
 		return FAILURE;
 	}
@@ -54,12 +145,21 @@ result make_board(struct board **b_pp, int w, int h)
 	return SUCCESS;
 }
 
-result make_state(struct state **s_pp)
+result make_state(struct state **s_pp, int w, int h)
 {
+	result r;
 	struct state *s = NULL;
+
 	s = (struct state *)malloc(sizeof(struct state));
 	if (s == NULL)
 	{
+		return FAILURE;
+	}
+
+	r = make_cells(s, w, h);
+	if (r == FAILURE)
+	{
+		free(s);
 		return FAILURE;
 	}
 
@@ -96,7 +196,8 @@ result make_cells(struct state *s_p, int w, int h)
 	{
 		for (j = 0; j < h; j++)
 		{
-			c[i][j] = rand_cell();
+			// c[i][j] = rand_cell();
+			c[i][j] = 0;
 		}
 	}
 
@@ -111,12 +212,14 @@ cell rand_cell(void)
 	return (cell)c;
 }
 
-void free_state(struct state *s_p)
+void free_state(struct board *b_p)
 {
-	free(s_p->_c);
-	s_p->_c = NULL;
-	free(s_p->c);
-	s_p->c = NULL;
+	free(b_p->s->_c);
+	b_p->s->_c = NULL;
+	free(b_p->s->c);
+	b_p->s->c = NULL;
+	free(b_p->s);
+	b_p->s = NULL;
 }
 
 void print_board(struct board *b)
--- C/lifegame.h
+++ C/lifegame.h
@@ -27,11 +27,14 @@ struct state
     cell *_c;
 };
 
+result next(struct board *b_p);
+int count_neighbor(int x, int y, struct board *b);
+cell toroidalize_cell(int x, int y, struct board *b);
 result make_board(struct board **b_pp, int w, int h);
-result make_state(struct state **s_pp);
+result make_state(struct state **s_pp, int w, int h);
 result make_cells(struct state *s_p, int w, int h);
 cell rand_cell(void);
-void free_state(struct state *s_p);
+void free_state(struct board *b_p);
 void print_board(struct board *b);
 
 #endif /* __LIFEGAME_H__ */
