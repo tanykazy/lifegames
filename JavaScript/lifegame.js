6b714f3447944cfc29920ee793e4903c7e1a8986
tanykazy <tanykazy@gmail.com>
2021-06-22 22:58:32 +0900

    Update lifegame.js

--- JavaScript/lifegame.js
+++ JavaScript/lifegame.js
@@ -5,33 +5,36 @@
  * @return {string} Next state of input
  */
 function lifegame(state) {
-    let current = JSON.parse(state);
-    let next = [];
-    for (let x = 0; x < current.length; x++) {
-        next[x] = [];
-        for (let y = 0; y < current[x].length; y++) {
-            let neighbor = current[(x + current.length - 1) % current.length][(y + current[x].length - 1) % current[x].length]
-                + current[(x + current.length - 1) % current.length][y]
-                + current[(x + current.length - 1) % current.length][(y + 1) % current[x].length]
-                + current[x][(y + current[x].length - 1) % current[x].length]
-                + current[x][(y + 1) % current[x].length]
-                + current[(x + 1) % current.length][(y + current[x].length - 1) % current[x].length]
-                + current[(x + 1) % current.length][y]
-                + current[(x + 1) % current.length][(y + 1) % current[x].length];
-            if (current[x][y] === 1) {
-                if (neighbor === 2 || neighbor === 3) {
-                    next[x][y] = 1;
+    const s = JSON.parse(state);
+    const n = [];
+    for (let x = 0; x < s.length; x++) {
+        n[x] = [];
+        for (let y = 0; y < s[x].length; y++) {
+            let c = 0;
+            const a = [-1, 0, 1];
+            for (const i of a) {
+                for (const j of a) {
+                    if (i != 0 || j != 0) {
+                        if (s[(x + i + s.length) % s.length][(y + j + s[x].length) % s[x].length] == 1) {
+                            c++;
+                        }
+                    }
+                }
+            }
+            if (s[x][y] === 1) {
+                if (c === 2 || c === 3) {
+                    n[x][y] = 1;
                 } else {
-                    next[x][y] = 0;
+                    n[x][y] = 0;
                 }
             } else {
-                if (neighbor === 3) {
-                    next[x][y] = 1;
+                if (c === 3) {
+                    n[x][y] = 1;
                 } else {
-                    next[x][y] = 0;
+                    n[x][y] = 0;
                 }
             }
         }
     }
-    return JSON.stringify(next);
+    return JSON.stringify(n);
 }
